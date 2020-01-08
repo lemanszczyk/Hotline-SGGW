1c5597bb870b22f53fba47e5dc138e688aa05346
Łukasz <lemanczyk98@gmail.com>
2020-01-08 13:36:49 +0100

    Shooting beta

---
+++ Assets/Scripts/shooting.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour
+{
+    //z którego miejsca strzelać
+    public Transform firePoint;
+
+    public GameObject bulletPrefab;
+
+    public float bulletForce = 20f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //kiedy naciskamy przycisk myszki
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Time.timeScale = 0.05f;
+            Shoot();
+        }
+    }
+    //podczas stzelania
+    void Shoot()
+    {
+        //tworzenie pocisku
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Debug.Log(firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        //dodawanie siły do pocisku
+        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+    }
+}
